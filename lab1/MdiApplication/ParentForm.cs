abd09cbefc67a58a4cc2efe741e0441986d5bcb8
Raxel55 <leha.ust@mail.ru>
2021-03-06 13:30:20 +0300

    Lab 2 - Task 3 completed

--- lab1/MdiApplication/ParentForm.cs
+++ lab1/MdiApplication/ParentForm.cs
@@ -40,5 +40,25 @@ namespace MdiApplication
             newChild.Show();
             newChild.Text = newChild.Text + " " + ++openDocuments;
         }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+
+        }
     }
 }
