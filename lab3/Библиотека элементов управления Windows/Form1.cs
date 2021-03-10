b8cb42eaecf74079b93c667492c5207590307b2c
Raxel55 <leha.ust@mail.ru>
2021-03-10 21:40:40 +0300

    Lab 3 -Task 1 completed

---
+++ lab3/Библиотека элементов управления Windows/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Библиотека_элементов_управления_Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControlTimer1.TimeEnabled = !userControlTimer1.TimeEnabled;
+        }
+    }
+}
