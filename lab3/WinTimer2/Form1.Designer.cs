3711caf198e8556dc6cef1cc72b8312862ba288d
Raxel55 <leha.ust@mail.ru>
2021-03-31 14:28:53 +0300

    Lab 3 - Task 2/3 completed

---
+++ lab3/WinTimer2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace WinTimer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer21 = new WinTimer2.UserControlTimer2();
+            this.clickButton1 = new WinTimer2.ClickButton();
+            this.SuspendLayout();
+            // 
+            // userControlTimer21
+            // 
+            this.userControlTimer21.Location = new System.Drawing.Point(24, 23);
+            this.userControlTimer21.Name = "userControlTimer21";
+            this.userControlTimer21.Size = new System.Drawing.Size(75, 23);
+            this.userControlTimer21.TabIndex = 0;
+            this.userControlTimer21.Text = "userControlTimer21";
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(24, 69);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(139, 63);
+            this.clickButton1.TabIndex = 1;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickButton1);
+            this.Controls.Add(this.userControlTimer21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer2 userControlTimer21;
+        private ClickButton clickButton1;
+    }
+}
+
